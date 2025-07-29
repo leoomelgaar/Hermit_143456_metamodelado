5ab24d13323fb7b3784238c80596e625586cfe4b
leoomelgaar <Leonardomelgar1@gmail.com>
2025-07-28 22:13:45 -0300

    Add accounting-consistente-corta new tests and undo OPT 2

--- src/main/java/org/semanticweb/HermiT/tableau/MetamodellingManager.java
+++ src/main/java/org/semanticweb/HermiT/tableau/MetamodellingManager.java
@@ -273,12 +273,11 @@ public final class MetamodellingManager {
 
     boolean checkInequalityMetamodellingRule() {
         boolean ruleApplied = false;
-        for (Map.Entry<Integer, List<Integer>> entry : differentIndividualsMap.entrySet()) {
-            Node node1 = this.m_tableau.getNode(entry.getKey());
-            for (Integer nodeId2 : entry.getValue()) {
-                Node node2 = this.m_tableau.getNode(nodeId2);
-                if (node1 != null && node2 != null && m_tableau.areDifferentIndividual(node1, node2) && checkInequalityMetamodellingRuleIteration(node1, node2))
-                    ruleApplied = true;
+        for (Node node1 : this.metamodellingNodes) {
+            for (Node node2 : this.metamodellingNodes) {
+                if (this.m_tableau.areDifferentIndividual(node1, node2)) {
+                    if (checkInequalityMetamodellingRuleIteration(node1, node2)) ruleApplied = true;
+                }
             }
         }
         return ruleApplied;
